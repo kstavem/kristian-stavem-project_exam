beac1e4e7a79fc1e94cc4e997ccd986a1b731e39
Kristian <kstavem@gmail.com>
2021-06-01 10:57:20 +0200

    menu work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-const burgerMenu = document.querySelector("#nav-toggler");
+const burgerMenu = document.querySelector("#nav--burger");
 const widthCheck = window.matchMedia("(min-width: 1025px)");
+const navButton = document.querySelector(".nav--button");
+const hiddenMenu = document.querySelector(".nav--toggle");
 
 widthCheck.addEventListener("change", (e) => {
     if (e.matches) {
         burgerMenu.checked = false;
+        hiddenMenu.classList.remove("nav--show");
     }
 });
+
+function toggleMenu() {
+    hiddenMenu.classList.toggle("nav--show");
+};
+
+navButton.addEventListener("click", toggleMenu);
